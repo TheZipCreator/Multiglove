8121b4e224afec9c5600ec321e0a8aab7d393acf
TheZipCreator <wooterxx09@gmail.com>
2018-05-16 17:30:01 -0400

    Update animation.lua

--- animation.lua
+++ animation.lua
@@ -1,25 +1,31 @@
 function createAnimation(textures)
   anim = {}
+  anim.delay = 1
   anim.frames = textures
   anim.curr = 1
   anim.playing = false
   anim.show = show
   anim.play = play
   anim.stop = stop
+  anim.count = 0
   return anim
 end
-function show(x, y)
-  love.graphics.draw(curr[i], x, y)
-  if playing == true then
-    curr = curr+1
-    if curr > #frames then
-      curr = 1
+function show(anm, x, y)
+  love.graphics.draw(anm.frames[anm.curr], x, y)
+  anm.count = anm.count+1
+  if anm.count > anm.delay then
+    anm.count = 0
+    if anm.playing == true then
+      anm.curr = anm.curr+1
+      if anm.curr > #anm.frames then
+        anm.curr = 1
+      end
     end
   end
 end
-function play()
-  playing = true
+function play(anm)
+  anm.playing = true
 end
-function stop()
-  playing = false
+function stop(anm)
+  anm.playing = false
 end
