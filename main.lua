2e4a6f7ec46af15b06c60e833e7b4587a7dd9bc2
TheZipCreator <wooterxx09@gmail.com>
2018-05-16 17:29:39 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,23 @@
 require 'animation'
-require 'talking'
+
 box = {}
 player = {}
 vol = 0
 onplat = false
+moving = false
 images = {}
 
 function love.load()
- 
   images[1] = love.graphics.newImage("data/back.png")
+  images[2] = love.graphics.newImage("data/player.png")
   player.x = 255
   player.y = 0
-  player.w = 10;
-  player.h = 10;
+  player.w = 32;
+  player.h = 96;
+  animframes = {love.graphics.newImage("data/p1.png"),love.graphics.newImage("data/p2.png"),love.graphics.newImage("data/p3.png"),love.graphics.newImage("data/p4.png"),love.graphics.newImage("data/p5.png"),love.graphics.newImage("data/p4.png"),love.graphics.newImage("data/p3.png"),love.graphics.newImage("data/p2.png")}
+  player.animation = createAnimation(animframes)
+  player.animation.playing = true
+  player.animation.delay = 5
   function coln(wallx,wallw,wally,wallh,playerx,playery,playerh,playerw)  
   if wallx < playerx + playerw and
     playerx < wallx + wallw and
@@ -30,24 +35,27 @@ function love.load()
       rv.h = h
       return rv
     end
-  for i=1,2 do
-    box[#box+1] = cb(i*355, 200, 255, 255)
+  for i=1,100 do
+    box[#box+1] = cb(i*455, 200, 255, 255)
   end
 end
 
 function love.update(dt)
+  moving = false
   new = {}
   new.x = player.x
   new.y = player.y
   vol = vol+0.1
   if love.keyboard.isDown('d') then
     new.x = new.x+5
+    moving = true
   elseif love.keyboard.isDown('a') then
     new.x = new.x-5
+    moving = true
   end
   onplat = false
   for i=1,#box do
-      if coln(box[i].x,box[i].w,box[i].y,box[i].h,new.x,new.y+vol,player.w,player.h) == true then
+      if coln(box[i].x,box[i].w,box[i].y,box[i].h,new.x,new.y+vol,player.h,player.w) == true then
         onplat = true
       end
   end
@@ -56,27 +64,19 @@ function love.update(dt)
   else
     vol = 0
   end
-  if love.keyboard.isDown('p') then
-  pos = pos+1
-  if pos > #dialouge  then
-    pos = 0
-  end
-  
   if love.keyboard.isDown('space') and onplat == true then
     vol = -3
   end
   if vol < 0 then
     new.y = new.y+vol
   end
-  end
   for i=1,#box do
-      if coln(box[i].x,box[i].w,box[i].y,box[i].h,new.x,new.y+vol,player.w,player.h) == false then
-        player.x = new.x
-        player.y = new.y
-      end
+    if coln(box[i].x,box[i].w,box[i].y,box[i].h,new.x,new.y,player.h,player.w) == false then
+      player.x = new.x
+      player.y = new.y
+    end
   end
 end
-
 function love.draw()
   for i=1,40 do
     love.graphics.draw(images[1], ((i-20)*800)-player.x/5, 0)
@@ -85,10 +85,10 @@ function love.draw()
   for i=1,#box do
     love.graphics.rectangle('fill',box[i].x,box[i].y,box[i].w,box[i].h)
   end
-  love.graphics.rectangle('fill', player.x, player.y, player.w, player.h)
-  
-  if pos < 1 then
-    love.graphics.rectangle('line',player.x+200,player.y-280,200,100)
+  if moving == false then
+    player.animation.curr = 2
   end
+  show(player.animation, player.x, player.y)
+  
   
 end
